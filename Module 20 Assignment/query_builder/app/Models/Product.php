16a1d3eefc8ed231dbf0596db3c5f9bbd909fe4d
Alamin <ma6033094@gmail.com>
2024-11-08 09:07:27 +0600

    Module 20 Assignment Project Setup

---
+++ Module 20 Assignment/query_builder/app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+
+    protected $table = 'products';
+    protected $fillable = [
+        'product_id',
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image'
+    ];
+
+    public static function validationRules()
+    {
+        return [
+            'product_id'         => 'required|string|unique:products',
+            'name'               => 'required|string',
+            'description'        => 'nullable',
+            'price'              => 'required|numeric',
+            'stock'              => 'nullable|integer',
+            'image'              => 'nullable|string',  
+        ];
+    }
+}
